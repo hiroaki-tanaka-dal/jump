db56832f6d12d6b2ccc0ece39bfe7a7799e4892d
hiroaki-tanaka-dal <156868105+hiroaki-tanaka-dal@users.noreply.github.com>
2026-09-06 15:01:54 +0900

    Add Panels sync package

---
+++ src/jump_pdf/panels/__init__.py
@@ -0,0 +1,13 @@
+from jump_pdf.panels.sync import (
+    SyncAction,
+    SyncItem,
+    build_sync_plan,
+    sync_pdfs,
+)
+
+__all__ = [
+    "SyncAction",
+    "SyncItem",
+    "build_sync_plan",
+    "sync_pdfs",
+]
